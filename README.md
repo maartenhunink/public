5494fb6f00bcfbed450d49fbf76c9713351571d1
maartenhunink <maartenhunink@gmail.com>
2013-04-20 21:46:23 +0200

    removed files I wont use

--- README.md
+++ README.md
@@ -4,4 +4,3 @@
 My public self.
 
 Feel free to fork this and make it your own public self.
-
--- tweets/data/js/payload_details.js
+++
@@ -1,4 +0,0 @@
-var payload_details =  {
-  "tweets" : 12103,
-  "created_at" : "Wed Jan 09 02:02:21 +0000 2013"
-}
